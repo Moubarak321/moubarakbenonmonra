5f3c628919e5449f56bf938dbc00b5c09bb5911a
Moubarak321 <benmoumass@gmail.com>
2025-09-02 10:15:56 +0100

    maj projets

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -2,7 +2,6 @@ import { Container } from "./styles";
 import githubIcon from "../../assets/github.svg"
 import DownloadApp from '../../assets/download.png'
 import externalLink from "../../assets/external-link.svg"
-import mbk from '../../assets/mbk.png'
 import ml from '../../assets/ml.png'
 import ds from '../../assets/ds.png'
 import djg from '../../assets/djg.png'
@@ -16,6 +15,31 @@ export function Project() {
       <h2>My Projects</h2>
       <div className="projects">
 
+<ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <div className="project-links">
+                <a href="https://ibtc-kohl.vercel.app/marketplace" target="_blank" rel="noreferrer">
+                  <img src={externalLink} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>BTIC</h3>
+              <p>
+                International Business Intelligence and Tourism Company. Découvrez notre entreprise et notre marketplace
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>NextJS</li>
+                <li>Firebase</li>
+                {/* <li>MachineLearning</li> */}
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
 
 
         <ScrollAnimation animateIn="flipInX">
@@ -42,6 +66,8 @@ export function Project() {
             </footer>
           </div>
         </ScrollAnimation>
+
+        
 
         <ScrollAnimation animateIn="flipInX">
           <div className="project">
